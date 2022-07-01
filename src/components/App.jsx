15d8992f11958f628df16b48d49ea024fc5c4316
michal-wolski <michal.wolski130@gmail.com>
2022-07-01 22:41:25 +0200

    Create navbar files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Homepage from './Homepage/Homepage';
 import Movies from './Movies/Movies';
+import Navbar from './Navbar/Navbar';
 
 export const App = () => {
   return (
     <>
+      <Navbar />
       <Homepage />
       <Movies />
     </>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="url">Homepage</a>
+        </li>
+        <li>
+          <a href="url">Movies</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
