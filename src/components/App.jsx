8a7611f56925eac4c62ed71b2cdf02389d4967a1
michal-wolski <michal.wolski130@gmail.com>
2022-07-01 22:20:08 +0200

    Create homepage files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
+import Homepage from './Homepage/Homepage';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Homepage />;
 };
