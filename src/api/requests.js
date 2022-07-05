deb9772c3edda39d9911f1bf95f8c7998671251e
michal-wolski <michal.wolski130@gmail.com>
2022-07-05 21:24:37 +0200

    Update requests.js

--- src/api/requests.js
+++ src/api/requests.js
@@ -5,3 +5,15 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const getTrendingMovies = async () =>
   await axios.get(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
+
+export const getSearchMovie = query =>
+  axios.get(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
+
+export const getMovieById = movieId =>
+  axios.get(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
+
+export const getMovieCast = movieId =>
+  axios.get(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`);
+
+export const getMovieReviews = movieId =>
+  axios.get(`${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}`);
